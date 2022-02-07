ec25d6d8508886da349a858a74221dd2585aa659
Hiroto0706 <mirunina2366@icloud.com>
2022-02-07 14:07:35 +0900

    cssを少しいじりました

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,7 +8,10 @@
     </div>
     <h1>
         <span>{{$post->title}}</span>
-        <a href="{{route('posts.edit', $post)}}">[編集する]</a>
+        <a href="{{route('posts.edit', $post)}}">[編集]</a>
+        <form method="post" action="">
+            <button class="button">[削除]</button>
+        </form>
     </h1>
     <div class="show_picture">
         <img src="{{asset('storage/' . $post->image)}}">
