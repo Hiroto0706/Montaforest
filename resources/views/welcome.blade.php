cfe3efd96b5e18ae47ef103ecf3b3235f9fb2765
Hiroto0706 <mirunina2366@icloud.com>
2022-03-03 14:08:25 +0900

    cssを修正しました

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,10 @@
         <a href="{{route('posts.create')}}">[追加]</a>
     </h1>
     <div class="kensaku-div">
-        <form action="{{url('/kensaku')}}" method="GET">
+        <form action="{{url('/kensaku')}}" method="GET" class="kensaku-form">
             <label for="">
                 タグで検索
-                <input type="text"  name="kensaku" class="kensaku-form">
+                <input type="text"  name="kensaku" class="kensaku-text">
                 <input type="submit" value="検索" class="kensaku-btn">
             </label>
         </form>
@@ -24,17 +24,20 @@
         @forelse ($posts as $post)
             <li>
                 No.{{$post->id}}
+                {{-- 詳細画面 --}}
                 <a href="{{route('posts.show', $post)}}">
                     {{$post->title}}
                 </a>
+                {{-- tags --}}
                 <div class="tags">
                     tag：
                     @foreach ($post->tags as $tag)
                     <a href="{{route('posts.showTag', $tag)}}">#{{$tag->tag_name}}</a>
-                @endforeach
+                    @endforeach
                 </div>
+                {{-- 画像 --}}
                 <div class="welcome_pic">
-                    <img src="{{asset('storage/' . $post->image)}}">
+                    <img class="show-pic" src="{{asset('storage/' . $post->image)}}">
                 </div>
             </li>
         @empty
